84cefc8448535299d6412b865ecc0aae6aed99c6
Kate Danielewicz <daneielewiczkate@gmail.com>
2026-09-05 21:28:32 -0700

    added a pixel bounding box to limit rasterizer math requirements

---
+++ tests/test_rasterizer.cpp
@@ -0,0 +1,8 @@
+// Unit tests for the rasterizer module (src/rasterizer.{hpp,cpp}).
+//
+// See test_SSE.cpp for the doctest cheatsheet.
+
+#include "doctest.h"
+#include "rasterizer.hpp"
+
+// TEST_CASEs go here.
